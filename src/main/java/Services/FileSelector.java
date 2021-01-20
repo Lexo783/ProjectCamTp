71fbbc949d5d407e86b34b4226aed2697d2b6774
Lexo783 <florentdebuchy@orange.fr>
2021-01-20 10:50:03 +0100

    Add service neural network && img for test selectorFiles

---
+++ src/main/java/Services/FileSelector.java
@@ -0,0 +1,31 @@
+package Services;
+
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class FileSelector {
+
+    private final DirectoryChooser directoryChooser = new DirectoryChooser();
+    private final FileChooser fileChooser = new FileChooser();
+
+    public File selectFiles(Stage primaryStage){
+        fileChooser.setTitle("Select");
+        return fileChooser.showOpenDialog(primaryStage);
+    }
+
+    public void selectDirectoryFiles()
+    {
+        configuringDirectoryChooser(directoryChooser);
+    }
+
+    private void configuringDirectoryChooser(DirectoryChooser directoryChooser) {
+        // Set title for DirectoryChooser
+        directoryChooser.setTitle("Select Some Directories");
+
+        // Set Initial Directory
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+    }
+}
